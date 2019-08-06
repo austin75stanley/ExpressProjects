1f1cde296f55dbef6211fb1c8f291b08b8160f1f
austin75stanley <austinstanleybi@gmail.com>
2019-08-06 14:12:03 +0300

    Removed _NA files

--- modelsNA/employee.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const employeeSchema = new mongoose.Schema({
-  fullname: {type: String},
-  email: {type: String},
-  mobile: {type: String},
-  city: {type: String},
-});
-mongoose.model=('Employee', employeeSchema);
